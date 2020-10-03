749f5c70f2b91246bacef86c4d4a5fd0c510e1aa
Aditya Chavan <adityachavan677@gmail.com>
2020-10-03 19:40:14 +0530

    My solutions to PASC Binary Search Contest.

---
+++ PASC_BINARY_SEARCH_CONTEST/AppyAndBalloons.cpp
@@ -0,0 +1,108 @@
+/*
+
+Appy loves balloons! She wants you to give her balloons on each of N consecutive days (numbered 1 through N); let's denote the number of balloons Appy wants on day i by Ai. The problem is that you only have M balloons. Fortunately, you can give her candies instead of balloons as well. On each day i, Appy accepts Bi candies per each balloon you do not give her — formally, if you give Appy Xi balloons on day i, then you have to give her Ci=max(0,Ai−Xi)⋅Bi candies as well.
+
+Your task is to minimize the maximum number of candies you give Appy on some day — find the minimum possible value of max(C1,C2,C3,…,CN).
+
+Input
+The first line of the input contains two space-separated integers N and M.
+The second line contains N space-separated integers A1,A2,…,AN.
+The third line contains N space-separated integers B1,B2,…,BN.
+Output
+Print a single line containing one integer — the minimum value of max(C1,C2,C3,…,CN).
+
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+#define N 100005
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+ll a[N],b[N];
+ll n,m;
+
+bool isPossible(ll mid)
+{
+	ll req = 0;
+	
+	for(int i = 0; i < n; i++)
+	{
+
+		ll temp = a[i] - (mid/b[i]);
+		
+		if(temp > 0)
+		{
+			req += temp;
+		}
+	}
+	 
+	 
+	 if(req > m)
+	 {
+	 	return false;
+	 }
+	 
+	 return true;
+}
+
+int BS(ll h)
+{
+	ll low = 0;
+	ll high = h;
+	
+	while(low < high)
+	{
+		ll mid = low + (high - low) / 2;
+		
+		if(isPossible(mid))
+		{
+			high = mid;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+	}
+	
+	cout << low << endl;
+	
+	return 0;
+	
+}
+
+int main() 
+{
+	// your code goes here
+	
+	ios_base::sync_with_stdio(0);cin.tie(0);
+ 
+    cin>>n>>m;
+ 
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+ 
+    for(int i=0;i<n;i++)
+    {
+        cin>>b[i];
+    }
+    
+    ll h = 0;
+    
+    for(int i=0;i<n;i++)
+    {
+        h = max(h, a[i]*b[i]);
+    }
+    
+    BS(h);
+	
+	return 0;
+}
+
+
+
+*/
---
+++ PASC_BINARY_SEARCH_CONTEST/JohnAndCows.cpp
@@ -0,0 +1,108 @@
+/*
+
+Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1,...,xN (0 <= xi <= 1,000,000,000).
+
+His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+Input
+t – the number of test cases, then t test cases follows.
+* Line 1: Two space-separated integers: N and C
+* Lines 2..N+1: Line i+1 contains an integer stall location, xi
+Output
+For each test case output one integer: the largest minimum distance.
+
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100005];
+
+bool canPlaceCows(int mid, int n, int k)
+{
+	// 1
+	int cows = 1; // placed one cow
+	int place = arr[0];  // placed at the first stalk value
+	
+	for(int i = 1; i < n; i++)
+	{
+		if(arr[i] - place >= mid)
+		{
+			cows += 1; // updated the number of cows placed
+			place = arr[i]; // update the stalk position of the newly placed cow
+		}
+	}
+	
+	if(cows >= k)
+	{
+		return true;
+	}
+	
+	return false;
+	 
+}
+
+int BS(int n, int k)
+{
+	int l = 1;
+	int h = arr[n - 1] - arr[0];
+	
+	while(h - l > 1)
+	{
+		int mid = l + (h - l) / 2;
+		
+		if(canPlaceCows(mid, n, k))
+		{
+			l = mid;
+		}
+		else
+		{
+			h = mid - 1;
+		}
+	}
+	
+	if(canPlaceCows(h, n, k))
+	{
+		return h;
+	}
+	
+	if(canPlaceCows(l, n, k))
+	{
+		return l;
+	}
+	
+	return 0;
+	
+}
+
+int main() 
+{
+	// your code goes here
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	
+	int t, n, k; // n --> no. of stalls k ---> no. of cows
+	cin>>t;
+	
+	while(t--)
+	{
+		cin >> n >> k;
+		
+		for(int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		
+		sort(arr, arr + n);
+		
+		cout << BS(n, k) << endl;
+		
+		
+		
+	}
+	
+	return 0;
+}
+
+
+*/
