4cef1042037d63c48a23c1d93160c5c88d645159
Aditya Chavan <adityachavan677@gmail.com>
2020-10-05 21:10:58 +0530

    solution to Job Sequencing problem

---
+++ Greedy/Job Sequencing/JS.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> vt = {{'a', 2, 100}, {'b', 1, 19}, {'c', 2, 27}, {'d', 1, 25}, {'e', 3, 15}};
+
+    const int N = vt.size();
+
+    sort(vt.begin(), vt.end(), [&](vector<int> &a, vector<int> &b) { return a[2] > b[2]; }); // Sort all jobs according to decreasing order of profit
+
+    vector<int> ans, slot;
+    ans.resize(N, 0);
+    slot.resize(N, 0);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = min(N, vt[i][1]) - 1; j >= 0; j--)
+        {
+            if (!slot[j]) // if available
+            {
+                slot[j] = 1;
+                ans[j] = i;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (slot[i])
+        {
+            cout << vt[ans[i]][0] << " ";
+        }
+    }
+
+    return 0;
+}
