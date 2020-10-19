56d0f5febf166922daeeb59d95ed99278b08b0f2
Aditya Chavan <adityachavan677@gmail.com>
2020-10-19 23:14:43 +0530

    added sliding window category problems

--- Leetcode/Sliding Window/1438.cpp
+++ Leetcode/Sliding Window/1438.cpp
@@ -1,6 +1,6 @@
 /*
 
-Given an array of integers nums and an integer limit, return the size of the longest non-empty 
+Given an array of integers nums and an integer limit, return the size of the longest  non-empty 
 subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
 */
