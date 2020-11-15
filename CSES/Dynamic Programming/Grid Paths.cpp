c36c8f8033e814a3869ab0953f9075f96756dc72
Aditya Chavan <adityachavan677@gmail.com>
2020-11-15 16:32:04 +0530

    easy problem

---
+++ CSES/Dynamic Programming/Grid Paths.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#define mod 1000000007
+#define int long long int
+using namespace std;
+
+// T.C : O(n^2)
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    vector<vector<char>> grid;
+    grid.resize(n, vector<char>(n, '-'));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> grid[i][j];
+        }
+    }
+
+    vector<vector<int>> dp;
+    dp.resize(n, vector<int>(n, 0));
+
+    dp[0][0] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (grid[i][j] == '.')
+            {
+                if (i > 0)
+                    (dp[i][j] += dp[i - 1][j]) % mod;
+
+                if (j > 0)
+                    (dp[i][j] += dp[i][j - 1]) % mod;
+            }
+            else
+            {
+                dp[i][j] = 0; // because there is trap here and zero b'coz we are also gonna add this value further
+            }
+        }
+    }
+
+    cout << dp[n - 1][n - 1] << " " << endl;
+
+    return 0;
+}
