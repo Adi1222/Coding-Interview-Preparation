8a8b1919d2355a32d04e4e178efb5a4e4c8b2c2b
Aditya Chavan <adityachavan677@gmail.com>
2021-05-18 16:23:16 +0530

    count substrgs variation : Atmost K unique chars

---
+++ Hash-Map/Count substrings having Atmost K Unique chars.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+// O(N)
+
+int main()
+{
+
+    string str = "aabcbcdbca";
+
+    int k = 2;
+
+    const int n = str.length();
+
+    int ans = 0;
+
+    int l = 0, r;
+
+    map<char, int> mp;
+
+    for (r = 0; r < n; r++)
+    {
+        mp[str[r]]++;
+
+        if (mp.size() <= k)
+        {
+            ans += (r - l + 1);
+        }
+        else
+        {
+            while (l < r && mp.size() > k)
+            {
+                mp[str[l]]--;
+
+                if (mp[str[l]] == 0)
+                {
+                    mp.erase(str[l]);
+                }
+
+                l++;
+
+                if (mp.size() <= k)
+                {
+                    ans += (r - l + 1);
+                }
+            }
+        }
+    }
+
+    cout << ans << " " << endl;
+
+    return 0;
+}
